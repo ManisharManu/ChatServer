5319a9d2a14c21ed865ee214274a8c7bea2f1880
Sayeed A P <35619101+sayeedfarooqui@users.noreply.github.com>
2018-04-03 13:55:16 +0530

    Update UDPClient.java

--- UDPClient.java
+++ UDPClient.java
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 2c263a1fda9698042c984669cade3d6e333e47d3
 import java.io.*;
 import java.net.*;
 class UDPClient
 {
-<<<<<<< HEAD
    public static void main(String args[]) throws Exception
    {
       BufferedReader inFromUser =
@@ -32,30 +27,4 @@ class UDPClient
         clientSocket.close();
    }
 
-=======
-	public static void main(String args[]) throws Exception
-	{
-	BufferedReader inFromUser =
-	new BufferedReader(new InputStreamReader(System.in));
-	DatagramSocket clientSocket = new DatagramSocket();
-	InetAddress IPAddress = InetAddress.getByName("localhost");
-	byte[] sendData = new byte[1024];
-	byte[] receiveData = new byte[1024];
-	boolean flag=true;
-	while(flag)
-	{
-		String sentence = inFromUser.readLine();
-		sendData = sentence.getBytes();
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-		clientSocket.send(sendPacket);
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		clientSocket.receive(receivePacket);
-		String modifiedSentence = new String(receivePacket.getData());
-		System.out.println("FROM SERVER:" + modifiedSentence);
-		if(sentence.equals("bye"))
-		flag=false;
-	}
-	clientSocket.close();
-	}
->>>>>>> 2c263a1fda9698042c984669cade3d6e333e47d3
 }
